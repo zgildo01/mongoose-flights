0f6a5caa876acf1e8a7b2f8f72941b95775f6a88
zgildo01 <zzaragozagildo@gmail.com>
2022-09-28 08:40:59 -0400

    add route and controller for meals

---
+++ controllers/meals.js
@@ -0,0 +1,15 @@
+import { Meal } from '../models/meal.js'
+
+function newMeal(req, res) {
+  Meal.find({})
+  .then(meals => {
+    res.render('meals/new', {
+      title: 'Add Meal',
+      meals,
+    })
+  })
+}
+
+export {
+  newMeal as new,
+}
